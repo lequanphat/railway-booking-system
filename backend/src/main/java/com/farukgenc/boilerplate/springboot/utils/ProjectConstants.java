e087d567bb12eac50c21557264bcc574134d346d
lequanphat3579@gmail.com <leuqnaphat3579@gmail.com>
2024-09-14 14:19:23 +0700

    change to vietnamese language

--- backend/src/main/java/com/farukgenc/boilerplate/springboot/utils/ProjectConstants.java
+++ backend/src/main/java/com/farukgenc/boilerplate/springboot/utils/ProjectConstants.java
@@ -13,7 +13,7 @@ public final class ProjectConstants {
 
 	public static final String DEFAULT_ENCODING = "UTF-8";
 
-	public static final Locale TURKISH_LOCALE = new Locale.Builder().setLanguage("tr").setRegion("TR").build();
+	public static final Locale VIETNAMESE_LOCALE = new Locale.Builder().setLanguage("vi").setRegion("VN").build();
 
 	private ProjectConstants() {
 
